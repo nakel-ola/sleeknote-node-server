77aeea8b003125dffe1a921b62a797e60860fb54
Olamilekan <nunuolamilekan@gmail.com>
2023-06-02 12:49:24 +0100

    updated server

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
 require("dotenv").config();
 import app from "./app";
-import initializeDB from "./db";
+// import initializeDB from "./db";
 
 const PORT = process.env.PORT || 4000;
-const main = async () => {
-  console.log("Connecting to database...");
+const start = async () => {
+  app.listen(PORT, () =>
+    console.log("listening on port http://localhost:" + PORT)
+  );
+  // console.log("Connecting to database...");
 
-  initializeDB
-    .initialize()
-    .then(() => {
-      console.log("Connected to database...");
-      app.listen(PORT, () =>
-        console.log("listening on port http://localhost:" + PORT)
-      );
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // initializeDB
+  //   .initialize()
+  //   .then(() => {
+  //     console.log("Connected to database...");
+  //     app.listen(PORT, () =>
+  //       console.log("listening on port http://localhost:" + PORT)
+  //     );
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 };
 
-main();
+start();
