6a7c5fb66435eb54a963aa19b332387970899df2
Olamilekan <nunuolamilekan@gmail.com>
2023-06-02 15:08:23 +0100

    changed database

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,23 +3,17 @@ import { DataSource } from "typeorm";
 import { User } from "./entities/User";
 
 const initializeDB = new DataSource({
-    type: "mysql",
-    host: process.env.MYSQL_HOST,
-    port: Number(process.env.MYSQL_PORT),
-    username: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    entities: [User],
-    synchronize: true,
-    logging: false,
-  });
-
-//   AppDataSource.initialize()
-//     .then(() => {
-//       console.log("Connected to mySql");
-//       // here you can start to work with your database
-//     })
-//     .catch((error) => console.log(error));
-// };
+  type: "postgres",
+  // host: process.env.MYSQL_HOST,
+  // port: Number(process.env.MYSQL_PORT),
+  // username: process.env.MYSQL_USERNAME,
+  // password: process.env.MYSQL_PASSWORD,
+  // database: process.env.MYSQL_DATABASE,
+  url: process.env.PGURL,
+  ssl: true,
+  entities: [User],
+  synchronize: true,
+  logging: false,
+});
 
 export default initializeDB;
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 require("dotenv").config();
 import app from "./app";
-// import initializeDB from "./db";
+import initializeDB from "./db";
 
 const PORT = process.env.PORT || 4000;
+
+// const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+// const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
 const start = async () => {
   try {
     // console.log("Connecting to database...");
 
-    // await initializeDB.initialize();
+    await initializeDB.initialize();
 
     app.listen(PORT, () =>
       console.log("listening on port http://localhost:" + PORT)
