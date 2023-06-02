81fe61486b8b44911ad06a58ed70cbec9c805af5
Olamilekan <nunuolamilekan@gmail.com>
2023-06-02 12:53:05 +0100

    added db initialization

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,21 @@
 require("dotenv").config();
 import app from "./app";
-// import initializeDB from "./db";
+import initializeDB from "./db";
 
 const PORT = process.env.PORT || 4000;
 const start = async () => {
-  app.listen(PORT, () =>
-    console.log("listening on port http://localhost:" + PORT)
-  );
-  // console.log("Connecting to database...");
+  try {
+    console.log("Connecting to database...");
 
-  // initializeDB
-  //   .initialize()
-  //   .then(() => {
-  //     console.log("Connected to database...");
-  //     app.listen(PORT, () =>
-  //       console.log("listening on port http://localhost:" + PORT)
-  //     );
-  //   })
-  //   .catch((error) => {
-  //     console.log(error);
-  //   });
+    await initializeDB.initialize();
+
+    app.listen(PORT, () =>
+      console.log("listening on port http://localhost:" + PORT)
+    );
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(error.message);
+  }
 };
 
 start();
