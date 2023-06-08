2109efc83691f9178fbe27f0aa724eeea49c8f1f
Olamilekan <nunuolamilekan@gmail.com>
2023-06-08 17:53:36 +0100

    updated entity path

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,7 +4,7 @@ import { DataSource } from "typeorm";
 const dataSource = new DataSource({
   type: "mysql",
   url: process.env.MYSQLURL,
-  entities: [__dirname + "/entities/*.entity.ts"],
+  entities: [__dirname + "/entities/*.entity.{js,ts}"],
   synchronize: true,
   logging: false,
 });
